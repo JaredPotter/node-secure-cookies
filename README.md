9489ddcaffe4ff75cd4f8da3ad80c5626da4add5
jaredpotter <jaredpotter1@gmail.com>
2019-12-20 13:03:43 -0700

    Add HTTPS. Add Redirect from HTTP to HTTPS. Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Getting Started
 
+1. openssl req -nodes -new -x509 -keyout server.key -out server.cert
+
+- Most answers don't matter but for 'Common Name' use `localhost`
+
+2. Once the certificate has been created you'll need to add to your operating system
+
+## macOS
+- open Keychain Access. 
+
+- File -> Import Items... and select the `server.cert` file.
+
+- Once imported, in the lower left hand corner of Keychain select 'Certificates'. 
+
+- Open your `localhost` certificate by double clicking.
+
+- Open the drop-down called 'Trust' and update 'When using this certificate:' to `Always Trust`. This will require administrator confirmation.
+
+## Windows
+
+TODO: add windows instructions
+
+## Continue Getting Started
+
+3. Run `npm install`
+4. Run `npm run start`
+
+## Notes
+- Safari only allows httpOnly cookies to be set for the same domain.
+
+Based in part:
+- https://flaviocopes.com/express-https-self-signed-certificate/
+- https://stackoverflow.com/questions/54836702/redirect-from-http-to-https-using-node-js-express
--- index.js
+++ index.js
@@ -3,77 +3,118 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
-const app = express();
-const port = 3000;
 
-const inMemoryUserStore = {
-    'jaredpotter': '<superSecretPassword>',
+const serverOptions = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
 };
 
-const whitelist = ['http://localhost:*', 'http://127.0.0.1:5501'];
+
+const inMemoryUserStoreDatabase = {
+    jaredpotter: '<superSecretPassword>'
+};
+
+const whitelist = [
+    'https://localhost:8080'
+];
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true);
+        console.log('Request from origin: ' + origin)
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
         } else {
-          callback(new Error('Not allowed by CORS'));
+            callback(new Error('Not allowed by CORS'));
         }
     },
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    credentials: true,
+    credentials: true
 };
-
-// Enable Express middlewares
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(cookieParser());
 
 const cookieConfig = {
     httpOnly: true, // to disable accessing cookie via client side js
-    //secure: true, // to force https (if you use it)
+    secure: true, // to force https (if you use it)
     maxAge: 1000000000 // ttl in ms (remove this option and cookie will die when browser is closed)
     // signed: true // if you use the secret with cookieParser
 };
 
-app.post('/login', (req, res) => {
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//          WEB SERVER
+const webApp = express();
+const webServerPort = 8080;
+webApp.use(express.static('public'));
+https.createServer(serverOptions, webApp).listen(webServerPort, () => {
+    console.log(`HTTPS Web Server Stated on port ${webServerPort}!`);
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const httpsApp = express();
+const httpsServerPort = 3000;
+
+// Enable Express middlewares
+httpsApp.use(cors(corsOptions));
+httpsApp.use(bodyParser.json());
+httpsApp.use(cookieParser());
+
+// HTTPS SERVER
+const httpsServer = https.createServer(serverOptions, httpsApp)
+httpsServer.listen(httpsServerPort, () => {
+    console.log(`HTTPS Server Stated on port ${httpsServerPort}!`);
+});
+
+httpsApp.post('/login', (req, res) => {
     const body = req.body;
     const username = body.username;
     const password = body.password;
 
-    if(inMemoryUserStore[username] === password) {
+    if (inMemoryUserStoreDatabase[username] === password) {
         // <Generate JWT token>
         const jwt = 'blah.blah.blah';
 
         res.cookie('token', jwt, cookieConfig);
         res.status(200).send('OK');
-    }
-    else {
+    } else {
         res.status(400).send('Username/password incorrect');
     }
 });
 
-app.post('/logout', (req, res) => {
+httpsApp.post('/logout', (req, res) => {
     res.clearCookie('token');
     res.send('OK');
 });
 
-app.get('/authenticatedData', (req, res) => {
+httpsApp.get('/authenticatedData', (req, res) => {
     const cookies = req.cookies;
     const token = cookies.token;
 
     // <verify token>
 
-    if(token) {
+    if (token) {
         // <if valid, return authenticated data>
         res.status(200).send('<authenticated data>');
-    }
-    else {
+    } else {
         res.status(400).send('Token missing or not valid');
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server Stated on port ${port}!`);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Redirect all HTTP to HTTPS
+const httpApp = express();
+httpApp.all('*', (req, res) => {
+    console.log('Redircting HTTP to HTTPS');
+    res.redirect(`https://localhost:${webServerPort}`);
 });
+
+const httpServer = http.createServer(httpApp);
+httpServer.listen(80, () =>
+    console.log(`HTTP Redirect server listening: http://localhost`)
+);
+
+
+
