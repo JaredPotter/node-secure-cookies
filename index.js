712c86e6eb6b8da636295de181b464a1299ad47e
jaredpotter <jaredpotter1@gmail.com>
2019-12-22 12:18:20 -1000

    Add domain and path.

--- index.js
+++ index.js
@@ -36,8 +36,10 @@ const corsOptions = {
 const cookieConfig = {
     httpOnly: true, // to disable accessing cookie via client side js
     secure: true, // to force https (if you use it)
-    maxAge: 1000000000 // ttl in ms (remove this option and cookie will die when browser is closed)
-    // signed: true // if you use the secret with cookieParser
+    maxAge: 1000000000, // ttl in ms (remove this option and cookie will die when browser is closed)
+    // signed: true, // if you use the secret with cookieParser
+    // domain: '', // 
+    // path: '', // 
 };
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
